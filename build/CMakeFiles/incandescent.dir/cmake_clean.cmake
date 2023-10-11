d38a616502056e26118755c78cd158505b797237
robjkezor <silin.mihael@gmail.com>
2023-10-11 11:06:56 +0300

    added entry point

---
+++ build/CMakeFiles/incandescent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/incandescent.dir/src/main.cpp.o"
+  "CMakeFiles/incandescent.dir/src/main.cpp.o.d"
+  "incandescent"
+  "incandescent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/incandescent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/incandescent.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for incandescent.
+# This may be replaced when dependencies are built.
